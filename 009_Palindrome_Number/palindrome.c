aa9ee9576bbb0097ea13472c28459b608db608c4
OPcheats <piyushpaul108@gmail.com>
2026-01-26 23:32:34 +0530

    Create palindrome.c

---
+++ 009_Palindrome_Number/palindrome.c
@@ -0,0 +1,17 @@
+bool isPalindrome(int x) {
+    
+    // negative numbers or numbers ending with 0 (except 0)
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+
+    int reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+
+    // even digits OR odd digits (ignore middle digit)
+    return (x == reversedHalf || x == reversedHalf / 10);
+}
