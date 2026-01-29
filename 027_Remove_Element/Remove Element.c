e25c98380196871cd4d4b70a4dec5bf1bbfebd8c
OPcheats <piyushpaul108@gmail.com>
2026-01-29 22:14:27 +0530

    Implement removeElement function to filter values

---
+++ 027_Remove_Element/Remove Element.c
@@ -0,0 +1,13 @@
+int removeElement(int* nums, int numsSize, int val) {
+
+    int k = 0;  // index for elements not equal to val
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
